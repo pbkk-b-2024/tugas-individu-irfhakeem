03d283a5bdf8a1ab59571c59b9f2d94d8b44121f
IrfanHakimm <mirfanhakim08@gmail.com>
2024-09-20 22:43:46 +0700

    update

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Fortify;
 
+use App\Models\Patient;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -20,16 +21,35 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input): User
     {
         Validator::make($input, [
+            'nik' => ['required', 'string', 'digits:8'],
             'name' => ['required', 'string', 'max:255'],
+            'tanggal_lahir' => ['required', 'date'],
+            'no_hp' => ['required', 'string', 'digits_between:1,12'],
+            'Golongan_darah' => ['required', 'string', 'max:2'],
+            'jenis_kelamin' => ['required', 'string', 'max:1'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        Patient::create([
+            'nik' => $input['nik'],
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'tanggal_lahir' => $input['tanggal_lahir'],
+            'no_hp' => $input['no_hp'],
+            'Golongan_darah' => $input['Golongan_darah'],
+            'jenis_kelamin' => $input['jenis_kelamin'],
+        ]);
+
+        $user->assignRole('patient');
+
+        return $user;
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,25 +8,69 @@
 
         <form method="POST" action="{{ route('register') }}">
             @csrf
+            <div class="grid grid-cols-2 gap-5">
 
-            <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+                <div>
+                    <x-label for="nik" value="{{ __('NIK') }}" />
+                    <x-input id="nik" pattern="[0-9]{8}" class="block mt-1 w-full" type="text" name="nik"
+                        :value="old('nik')" required autofocus autocomplete="off" />
+                </div>
 
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
+                <div>
+                    <x-label for="name" value="{{ __('Name') }}" />
+                    <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
+                        required autofocus autocomplete="off" />
+                </div class="mt-4">
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+                <div class="mt-4">
+                    <x-label for="tanggal_lahir" value="{{ __('Tanggal Lahir') }}" />
+                    <x-input id="tanggal_lahir" class="block mt-1 w-full" type="date" name="tanggal_lahir"
+                        :value="old('tanggal_lahir')" required autofocus autocomplete="off" />
+                </div class="mt-4">
 
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <div class="mt-4">
+                    <x-label for="no_hp" value="{{ __('No Hp') }}" />
+                    <x-input id="no_hp" pattern="^08[0-9]{8,13}$" class="block mt-1 w-full" :value="old('no_hp')"
+                        type="text" name="no_hp" required autofocus autocomplete="off" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="Golongan_darah" value="{{ __('Golongan Darah') }}" />
+                    <select id="Golongan_darah" class="block mt-1 w-full" name="Golongan_darah"
+                        :value="old('Golongan_darah')" required autofocus>
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                        <option value="AB">AB</option>
+                        <option value="O">O</option>
+                    </select>
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="jenis_kelamin" value="{{ __('Jenis Kelamin') }}" />
+                    <select id="jenis_kelamin" class="block mt-1 w-full" name="jenis_kelamin"
+                        :value="old('jenis_kelamin')" required autofocus>
+                        <option value="L">L</option>
+                        <option value="P">P</option>
+                    </select>
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                        required autocomplete="off" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="password" value="{{ __('Password') }}" />
+                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                        autocomplete="off" />
+                </div>
+
+                <div class="mt-4">
+                    <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                    <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                        name="password_confirmation" required autocomplete="off" />
+                </div>
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
@@ -37,8 +81,18 @@
 
                             <div class="ms-2">
                                 {!! __('I agree to the :terms_of_service and :privacy_policy', [
-                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
-                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
+                                    'terms_of_service' =>
+                                        '<a target="_blank" href="' .
+                                        route('terms.show') .
+                                        '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' .
+                                        __('Terms of Service') .
+                                        '</a>',
+                                    'privacy_policy' =>
+                                        '<a target="_blank" href="' .
+                                        route('policy.show') .
+                                        '" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">' .
+                                        __('Privacy Policy') .
+                                        '</a>',
                                 ]) !!}
                             </div>
                         </div>
@@ -47,7 +101,8 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
